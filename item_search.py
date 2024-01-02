999ec6949b7b6abe69d34a3ca5335a15c217481e
Aron-Mathias <saararonmathias1@gmail.com>
2024-01-03 00:10:40 +0200

    Refactor item_info initialization and add additional attributes

--- item_search.py
+++ item_search.py
@@ -1,4 +1,5 @@
 import asyncio
+import glob
 import os
 import time
 
@@ -9,20 +10,29 @@ from bs4 import BeautifulSoup
 
 
 class ItemInfo:
-    def __init__(self) -> None:
-        self.description = ""
-        self.chi = ""
-        self.splicing_seed1 = ""
-        self.splicing_seed2 = ""
-        self.equip_playmod_text = ""
-        self.unequip_playmod_text = ""
-        self.playmod_name = ""
-
+    def __init__(self, attrs: dict[str]) -> None:
+        self.description = attrs["description"].strip()
+        self.type = attrs["item_type"].strip()
+        self.chi = attrs["chi"].strip()
+        self.texture_type = attrs["texture_type"].strip()
+        self.collision_type = attrs["collision_type"].strip()
+        self.hardness = attrs["hardness"].strip()
+        self.seed_colours = attrs["seed_colours"].strip()
+        self.grow_time = attrs["grow_time"].strip()
+        self.default_gem_drop = attrs["default_gem_drop"].strip()
+        self.splicing_seed1 = attrs["splicing_seed1"].strip()
+        self.splicing_seed2 = attrs["splicing_seed2"].strip()
+        self.equip_playmod_text = attrs["equip_playmod_text"].strip()
+        self.unequip_playmod_text = attrs["unequip_playmod_text"].strip()
+        self.playmod_name = attrs["playmod_name"].strip()
 
 async def create_item_names():
-    items_data = growtopia.ItemsData("")  # Specify the path to your items.dat file here
+    dat_files = glob.glob("**/*.dat", recursive=True)
+    items_data = growtopia.ItemsData(
+        dat_files[0]
+    )
     await items_data.parse()
-    with open("item_names.txt", "w") as f:
+    with open("data/item_names.txt", "w") as f:
         f.write("\n".join([item.name for item in items_data.items]))
 
 
@@ -30,6 +40,7 @@ def find_formatted_name(item: str, file_content: str):
     for line in file_content.split("\n"):
         if line.lower().startswith(item):
             return line
+
 
 @alru_cache
 async def find_item(item: str):
@@ -41,50 +52,81 @@ async def find_item(item: str):
     await session.close()
     soup = BeautifulSoup(data, "html.parser")
 
-    try:
-        description = soup.find("div", {"class": "card-text"}).text
+    attributes = ['item_type', 'chi', 'texture_type', 'collision_type', 'hardness', 'seed_colours', 'grow_time', 'default_gem_drop']
+    results = {}
 
-        if "|" in description:
-            description = description.replace("|", " ")
+    try:
+        results["description"] = soup.find("div", {"class": "card-text"}).text
+
+        if "|" in results["description"]:
+            results["description"] = results["description"].replace("|", " ")
     except AttributeError:
-        description = "This item has no description!"
+        results["description"] = "This item has no description!"
 
     try:
-        splicing_seeds = (
-        soup.find(attrs={"class": "recipebox", "style": "background: #66cc66; color: #006600;"})
+        data_field = soup.find(attrs={"class": "card-field"})
+    except Exception as e:
+        print(e)
+        return
+
+    # Finding each of the attributes in the card-field class (type, chi, hardness, etc)
+    for i, attr in enumerate(attributes):
+        try:
+            node = data_field.find("tbody").find("tr")
+            for _ in range(i):
+                node = node.find_next("tr")
+            results[attr] = node.find("td").text
+        except AttributeError:
+            results[attr] = "None"
+
+    # Some stupid space missing from webscraping the hardness
+    if 'hardness' in results:
+        results['hardness'] = results['hardness'].replace("HitsRestores", "Hits. Restores")
+
+    # Finding the splicing seeds
+    try:
+        splicing_seeds = soup.find(
+            attrs={
+                "class": "recipebox",
+                "style": "background: #66cc66; color: #006600;",
+            }
         )
 
-        splicing_seed1 = splicing_seeds.find("tr").find_next("tr").find_next("tr").find("a").text
-        splicing_seed2 = splicing_seeds.find("tr").find_next("tr").find_next("tr").find("a").find_next("a").text
-
-        if "seed" not in splicing_seed1.lower():
-            splicing_seed1 = "Blank"
-        if "seed" not in splicing_seed2.lower():
-            splicing_seed2 = "Blank"
-
+        results["splicing_seed1"] = (
+            splicing_seeds.find("tr").find_next("tr").find_next("tr").find("a").text
+        )
+        results["splicing_seed2"] = (
+            splicing_seeds.find("tr")
+            .find_next("tr")
+            .find_next("tr")
+            .find("a")
+            .find_next("a")
+            .text
+        )
     except AttributeError:
-        splicing_seed1 = "Blank"
-        splicing_seed2 = "Blank"
+        results["splicing_seed1"] = "Blank"
+        results["splicing_seed2"] = "Blank"
 
+    # Finding the equip playmod text
     try:
-        equip_playmod_text = soup.find(
+        results["equip_playmod_text"] = soup.find(
             "td", {"style": "color:#199e24; padding:0px 20px; font-style: italic"}
         ).text
-
     except AttributeError:
-        equip_playmod_text = ""
+        results["equip_playmod_text"] = ""
 
+    # Finding the unequip playmod text
     try:
-        unequip_playmod_text = soup.find(
+        results["unequip_playmod_text"] = soup.find(
             "td", {"style": "color:#9e2a18; padding:0px 20px; font-style: italic"}
         ).text
-        
     except AttributeError:
-        unequip_playmod_text = ""
+        results["unequip_playmod_text"] = ""
 
+    # Finding the playmod name
     try:
-        if equip_playmod_text != "" and unequip_playmod_text != "":
-            playmod_name = (
+        if results["equip_playmod_text"] != "" and results["unequip_playmod_text"] != "":
+            results["playmod_name"] = (
                 soup.find("div", {"class": "mw-parser-output"})
                 .find("p")
                 .find_next("p")
@@ -92,37 +134,20 @@ async def find_item(item: str):
                 .text
             )
         else:
-            playmod_name = ""
+            results["playmod_name"] = ""
     except AttributeError:
-        playmod_name = ""
+        results["playmod_name"] = ""
 
-    try:
-        if len((list_of_contents := soup.find("td").find_next("td").contents)) >= 2:
-            list_of_contents = 1
-        else:
-            list_of_contents = 0
-        chi = soup.find("td").find_next("td").contents[list_of_contents].text.strip()
-    except AttributeError:
-        chi = "None"
-
-    item_info = ItemInfo()
-    item_info.description = description
-    item_info.chi = chi
-    item_info.splicing_seed1 = splicing_seed1
-    item_info.splicing_seed2 = splicing_seed2
-    item_info.equip_playmod_text = equip_playmod_text
-    item_info.unequip_playmod_text = unequip_playmod_text
-    item_info.playmod_name = playmod_name
+    item_info = ItemInfo(results)
     return item_info
-
 
 if __name__ == "__main__":
     while True:
         try:
-            if "item_names.txt" not in os.listdir():
+            if "data/item_names.txt" not in os.listdir():
                 asyncio.run(create_item_names())
 
-            with open("item_names.txt", "r") as f:
+            with open("data/item_names.txt", "r") as f:
                 file_content = f.read()
 
             inp = input("Enter item name: ")
@@ -131,12 +156,16 @@ if __name__ == "__main__":
             if correct_name:
                 item_info = asyncio.run(find_item(correct_name))
                 print(f"Description: {item_info.description}")
+                print(f"Type: {item_info.type}")
                 print(f"Chi: {item_info.chi}")
-                if (
-                    item_info.splicing_seed1 == "Blank"
-                    and item_info.splicing_seed2 == "Blank"
-                ):
-                    print(f"This item can't be spliced")
+                print(f"Texture type: {item_info.texture_type}")
+                print(f"Collision type: {item_info.collision_type}")
+                print(f"Item hardness: {item_info.hardness}")
+                print(f"Seed colours: {item_info.seed_colours}")
+                print(f"Grow time: {item_info.grow_time}")
+                print(f"Default gem drop: {item_info.default_gem_drop}")
+                if item_info.splicing_seed1 == "Blank" and item_info.splicing_seed2 == "Blank":
+                    print(f"This item cannot be spliced")
                 else:
                     print(f"Splicing seed 1: {item_info.splicing_seed1}")
                     print(f"Splicing seed 2: {item_info.splicing_seed2}")
@@ -147,7 +176,9 @@ if __name__ == "__main__":
                 if item_info.unequip_playmod_text == "":
                     print(f"This item has no playmod text upon unequipping")
                 else:
-                    print(f"Unequip playmod text: {item_info.unequip_playmod_text.strip()}")
+                    print(
+                        f"Unequip playmod text: {item_info.unequip_playmod_text.strip()}"
+                    )
                 if item_info.playmod_name == "":
                     print(f"This item has no playmod")
                 else:
